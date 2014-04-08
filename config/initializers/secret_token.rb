87328b3800519d9c71e45d681f4423fa0bc7c790
samarthasthana <samarth.asthana@gmail.com>
2014-04-07 20:12:43 -0400

    Rails Gen commit 4/7

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WolfPackGuide::Application.config.secret_key_base = '887243a21c194e2606032bf86f99b32b93d7854bc6ebab578cf3ff998432af5cb847829719bd06bbd3434c08310de6ffdf53e44571c47d0b2b1fe1d2db2fe52a'
